e809bc138b6c6446f4c1b97d9c2df5f3495ecf26
SheriffMudasir <sheriffmudasir2021@gmail.com>
2024-12-17 14:21:13 +0100

    Setting up light productuion server using gunicorn

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-14goq@)_6vf(&!eu9^6dx6rgg5a&ozgsj##$3caz@s&v053(7%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
